acee34a5ab9042249d8a6019394bceb3d124210c
BasuKlizos <basudev@klizos.com>
2025-02-25 19:56:26 +0530

    successfully connected to websocket

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
-async def index():
-    return JSONResponse(content={"msg":"I am Healthy."})
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+active_connections: List[WebSocket] = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("---------------connected")
+    active_connections.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for connection in active_connections:
+                await connection.send_text(data)  # Broadcast message to all clients
+    except:
+        active_connections.remove(websocket)
