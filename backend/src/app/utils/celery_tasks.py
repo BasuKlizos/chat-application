5176ff5820299e10a68e49fdae1d31f89bbf8c5b
basu <samantabasu.2001@gmail.com>
2025-03-04 10:35:14 +0530

    changes for celery taks

--- backend/src/app/utils/celery_tasks.py
+++ backend/src/app/utils/celery_tasks.py
@@ -7,10 +7,25 @@ from src.app.models.message_models import Message
 
 class MessageTasks:
     @staticmethod
-    @celery_app.task(name="Store chats to db")
+    @celery_app.task(name="Store chats to db", ignore_result=True)
     def store_messages(sender_id: str, receiver_id: str, message: str):
-        # return asyncio.run(Message.save_messaage(sender_id, receiver_id, message))
-        return async_to_sync(Message.save_messaage)(sender_id, receiver_id, message)
+        from eventlet import greenthread
+        greenthread.spawn_n(asyncio.run, Message.save_messaage(sender_id, receiver_id, message))
+
+    # @celery_app.task(name="Store chats to db")
+    # def store_messages(sender_id: str, receiver_id: str, message: str):
+    #     # return asyncio.run(Message.save_messaage(sender_id, receiver_id, message))
+    #     # return async_to_sync(Message.save_messaage)(sender_id, receiver_id, message)
+    #     loop = asyncio.new_event_loop()
+    #     try:
+    #         asyncio.set_event_loop(loop)
+    #         result = loop.run_until_complete(
+    #             Message.save_messaage(sender_id, receiver_id, message)
+    #         )
+    #     finally:
+    #         loop.close()
+    #         asyncio.set_event_loop(None)
+    #     return result
 
     # @staticmethod
     # @celery_app.task(name="fetch-chats-from-db")
