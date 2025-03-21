8d6b8ba5ab43ea27522188ca586510a7be6be25e
BasuKlizos <basudev@klizos.com>
2025-03-21 19:07:16 +0530

    code cleaned completed

--- backend/src/app/utils/jwt.py
+++ backend/src/app/utils/jwt.py
@@ -40,21 +40,3 @@ class JWTAuth:
         except Exception as e:
             print(f"Unexpected error encountered: {e}")
             return None
-
-    # @classmethod
-    # def get_current_user(cls, token: str = Depends(oauth2_scheme)):
-    #     try:
-    #         payload = jwt.decode(token, cls.JWT_SECRET_KEY,
-    #           algorithms=[cls.ALGORITHM])
-    #         username = payload.get("username")
-    #         if not username:
-    #             raise HTTPException(status_code=401, detail="Username not found")
-    #         return username
-    #     except JWTError:
-    #         raise HTTPException(status_code=401, detail="Token is invalid or
-    #                                                               expired.")
-    #     except Exception as e:
-    #         raise HTTPException(
-    #             status_code=500,
-    #             detail=f"An error occurred while decoding the token: {e}",
-    #         )
